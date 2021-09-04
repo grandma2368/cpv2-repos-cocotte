4f3f12d970e1946c5b4aea8ca6ebe0424b06a8e7
grandma2368 <46317358+grandma2368@users.noreply.github.com>
2021-09-04 14:10:37 +0200

    assignation donnees dans data
    assignation dans data faite

--- cpv2.py
+++ cpv2.py
@@ -12,11 +12,9 @@ for line in fileinput.input():
     #verifie si l'utilisateur souhaite quitter le programme et agit en consequence
     if line.rstrip() == "exit":
         sys.exit()
-    parsing.parsing(line.rstrip())
+    parsing.parsing(line.rstrip(), data)
     #IL FAUT FAIRE DU PARSING ICI --> FAIRE UNE FONCTION DEDIEE
     #IL FAUT CHECKER SI LA VARIABLE EXISTE DEJA --> SI EXISTE REASSIGNER --> SINON CREER ENTREE ET LUI ASSIGNER VALEUR
-#code qui suit n est la que pour tester et comprendre comment fonctionne python
-    print("Test: ", line.rstrip())
-    data.append(line.rstrip())
-    for truc in data:
-        print("il y a dans data --> ", truc)
+    for test in data:
+        print("varName = ", test[0].replace(" ", ""))
+        print("varValue = ", test[1].replace(" ", ""))
--- parsing.py
+++ parsing.py
@@ -1,5 +1,21 @@
-def parsing(line):
-    if len(line.split("=")) == 1:
+def newVarInData(varName, varValue, data):
+    datum = [varName, varValue]
+    data.append(datum)
+
+def isAlreadyExist(varName, data):
+    for eachVar in data:
+        if eachVar[0] == varName:
+            print("existe dans data dans fonction isAlreadyExist")
+            print(eachVar[0])
+            return
+        else:
+            print("n existe pas dans Data pour le moment")
+    
+
+def parsing(line, data):
+    res = line.split("=")
+    if len(res) == 1:
+        isAlreadyExist(line.replace(" ", ""), data)
         #RECHERCHER SI LA VARIABLE DEMANDEE EXISTE DANS DATA --> SI OUI L AFFICHER --> SINON RENVOYER UN MESSAGE D ERREUR
         #RENVOYER ICI UN INT POUR FAIRE LE TRAITEMENT DE CA DEPUIS DATA DIRECTEMENT DANS cpv2.py
         print(line)
@@ -7,4 +23,5 @@ def parsing(line):
         #VERIFIER S IL S'AGIT D UN POLYNOME A RESOUDRE OU D UNE VARIABLE A ASSIGNER
         #CHECKER S IL Y A UNE VALEUR DEJA DE DONNEE OU S'IL FAUT LA CALCULER
         #VOIR SI C'EST UNE ASSIGNATION PAR RAPPORT A UNE VARIABLE DEJA EXISTANTE
-        print("il y aura un autre parsing a faire en plus")
+        #ENTRER LA VARIABLE DANS DATA
+        newVarInData(res[0].replace(" ", ""), res[1].replace(" ", ""), data)
