7df1105839582adf3537d27c3c6998b1fd3d34d6
grandma2368 <46317358+grandma2368@users.noreply.github.com>
2021-09-07 22:42:49 +0200

    dodo
    dodo

--- parsing.py
+++ parsing.py
@@ -1,3 +1,15 @@
+#cherche si la variable existe deja dans data et dans ce cas la reassigne
+def isReassignated(varName, varValue, data):
+    value = varValue.lower()
+    name = varName.lower()
+
+    for eachVar in data:
+        if eachVar[0] == name:
+            #reassigned la nouvelle valeur
+            eachVar[1] = value
+            print(eachVar[1])
+            return("reassigned")
+
 #check le nom de la fonction et si la fonction a bien une variable
 def checkFunction(varName, varValue, data):
     res = varName.split('(')
@@ -11,30 +23,31 @@ def checkFunction(varName, varValue, data):
         return("error")
     print(varValue)
 
+#cree une nouvelle variable si les donnees sont correctes
 def newVarInData(varName, varValue, data):
-    varValue.lower()
-    varName.lower()
+    value = varValue.lower()
+    name = varName.lower()
 
     #recherche si le nom de variable ne contient que des lettres ou non
     i = 0
-    lenght = len(varName)
+    lenght = len(value)
     while i < lenght:
         agree = "azertyuiopqsdfghjklmwxcvbn()"
-        if varName[i] in agree:
+        if name[i] in agree:
             i += 1
         else:
             #le nom de la variable ne contient pas que des lettres --> cas d'erreur
             return("error")
-    if '(' in varName:
+    if '(' in name:
         #CHECKER LA FONCTION ET FAIRE L'ASSIGNATION DE FONCTION
-        checkFunction(varName, varValue, data)
+        checkFunction(name, value, data)
         return
     #recherche si la variable est à calculer à partir d'autre ou non ou si matrice
     i = 0
-    lenght = len(varValue)
+    lenght = len(value)
     while i < lenght:
         agree = "1234567890+-/*i"
-        if varValue[i] in agree:
+        if value[i] in agree:
             i += 1
         else:
             #RECHERCHER LES VARIABLES DANS DATA
@@ -42,33 +55,33 @@ def newVarInData(varName, varValue, data):
             print("pas un calcul ou une variable de deja donnee")
             return
     #enregistre la variable dans data
-    datum = [varName, varValue]
+    datum = [name, value]
     data.append(datum)
-    #POUR LE TEST
-    print("variable bien enregistree")
+    print(value)
 
+#renvoie la valeur de la variable si elle est trouvee dans data
 def isAlreadyExist(varName, data):
-    varName.lower()
+    name = varName.lower()
     for eachVar in data:
-        if eachVar[0] == varName:
-            print("existe dans data dans fonction isAlreadyExist")
-            print(eachVar[0])
+        if eachVar[0] == name:
+            #renvoie la valeur de la variable trouvee
+            print(eachVar[1])
             return
         else:
-            print("n existe pas dans Data pour le moment")
+            #renvoie une erreur car n'a pas trouve la variable dans data
+            print("Cette variable n'a pas encore ete enregistree")
     
 
 def parsing(line, data):
     res = line.split("=")
     if len(res) == 1:
         isAlreadyExist(line.replace(" ", ""), data)
-        #RECHERCHER SI LA VARIABLE DEMANDEE EXISTE DANS DATA --> SI OUI L AFFICHER --> SINON RENVOYER UN MESSAGE D ERREUR
-        #RENVOYER ICI UN INT POUR FAIRE LE TRAITEMENT DE CA DEPUIS DATA DIRECTEMENT DANS cpv2.py
-        print(line)
     else:
-        #VERIFIER S IL S'AGIT D UN POLYNOME A RESOUDRE OU D UNE VARIABLE A ASSIGNER
-        #CHECKER S IL Y A UNE VALEUR DEJA DE DONNEE OU S'IL FAUT LA CALCULER
-        #VOIR SI C'EST UNE ASSIGNATION PAR RAPPORT A UNE VARIABLE DEJA EXISTANTE
-        #ENTRER LA VARIABLE DANS DATA
-        if newVarInData(res[0].replace(" ", ""), res[1].replace(" ", ""), data) == "error":
-            return("error")
+        #VERIFIER S'IL S'AGIT OU NON D'UNE REASSIGNATION DE VARIABLE
+        if isReassignated(res[0].replace(" ", ""), res[1].replace(" ", ""), data) != "reassigned":
+            #VERIFIER S IL S'AGIT D UN POLYNOME A RESOUDRE OU D UNE VARIABLE A ASSIGNER
+            #CHECKER S IL Y A UNE VALEUR DEJA DE DONNEE OU S'IL FAUT LA CALCULER
+            #VOIR SI C'EST UNE ASSIGNATION PAR RAPPORT A UNE VARIABLE DEJA EXISTANTE
+            #ENTRER LA VARIABLE DANS DATA
+            if newVarInData(res[0].replace(" ", ""), res[1].replace(" ", ""), data) == "error":
+                return("error")
