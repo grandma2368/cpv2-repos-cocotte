7166519d1f52e3df8d7e977dd58909a56a85b49d
grandma2368 <46317358+grandma2368@users.noreply.github.com>
2021-09-08 22:11:11 +0200

    course
    course

--- calcul.py
+++ calcul.py
@@ -1,3 +1,22 @@
+#cherche les parentheses prioritaires
+def checkParenthesis(exp):
+    #recupere l'indice de la parenthese ouvrante prioritaire
+    prtOpen = 0
+    #vois s'il y a une parenthese fermante
+    prtClosed = 0
+    i = 0
+    for prt in exp:
+        if prt == '(':
+            prtOpen = i
+        if prt == ')':
+            prtClosed = i
+            #RENVOYER VERS UNE FONCTION QUI VA CALCULER L'ENTRE DEUX
+        i += i
+    if prtClosed != 0:
+        checkParenthesis(exp)
+
 #calcule une expression pour l'enregistrer dans data
 def calculate(exp, data):
+    checkParenthesis(exp)
+    #FAIRE UNE FONCTION POUR CALCULER CE QUI RESTE DANS EXP
     return("error")
