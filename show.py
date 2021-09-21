d85f75e8ee55a893103ac937b5accd9c93eefdbd
camille duverger <camilleduverger@camilles-MacBook-Pro.local>
2021-09-21 09:00:45 +0200

    mardi matin

--- show.py
+++ show.py
@@ -66,39 +66,59 @@ def showAllData(data):
 def showDatum(data, name):
     found = 0
     for vrb in data:
-        if vrb[0] == name:
-            found = 1
+        if type(vrb[1]) == list:
+            i = 1
             tmp = str(vrb[1])
-            i = 0
             lenght = len(tmp)
-            fnd = 0
-            exp = ''
             while i < lenght:
                 cur = ''
-                while i < lenght and utils.checkChr(tmp[i], "0123456789.") == 0:
-                    cur = cur + tmp[i]
-                    i += 1
-                    fnd = 1
-                if fnd == 1:
-                    exp = exp + cur + ' '
+                while tmp[i] != ']':
+                    if tmp[i] == "'":
+                        i += 1
+                    else:
+                        cur = cur + tmp[i]
+                        i += 1
+                cur = cur + tmp[i]
+                print(cur)
+                i += 1
+                if tmp[i] == ']':
+                    break
+                else:
+                    i += 2
+        else:
+            if vrb[0] == name:
+                found = 1
+                tmp = str(vrb[1])
+                i = 0
+                lenght = len(tmp)
+                fnd = 0
+                exp = ''
+                while i < lenght:
                     cur = ''
-                    fnd = 0
-                while i < lenght and utils.checkChr(tmp[i], "%^*()-+/") == 0:
-                    cur = cur + tmp[i]
-                    i += 1
-                    fnd = 1
-                if fnd == 1:
-                    exp = exp + cur + ' '
-                    cur = ''
-                    fnd = 0
-                while i < lenght and utils.checkChr(tmp[i], "qwertyuiopasdfghjkklzxcvbnm()") == 0:
-                    cur = cur + tmp[i]
-                    i += 1
-                    fnd = 1
-                if fnd == 1:
-                    exp = exp + cur + ' '
-                    cur = ''
-                    fnd = 0
+                    while i < lenght and utils.checkChr(tmp[i], "0123456789.") == 0:
+                        cur = cur + tmp[i]
+                        i += 1
+                        fnd = 1
+                    if fnd == 1:
+                        exp = exp + cur + ' '
+                        cur = ''
+                        fnd = 0
+                    while i < lenght and utils.checkChr(tmp[i], "%^*()-+/") == 0:
+                        cur = cur + tmp[i]
+                        i += 1
+                        fnd = 1
+                    if fnd == 1:
+                        exp = exp + cur + ' '
+                        cur = ''
+                        fnd = 0
+                    while i < lenght and utils.checkChr(tmp[i], "qwertyuiopasdfghjkklzxcvbnm()") == 0:
+                        cur = cur + tmp[i]
+                        i += 1
+                        fnd = 1
+                    if fnd == 1:
+                        exp = exp + cur + ' '
+                        cur = ''
+                        fnd = 0
     if found == 0:
         print("La variable '" + name + "' n'a pas encore ete assignee.")
     else:
