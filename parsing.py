542075b050033bee3f4d41976bb17b535a6cdc0d
cduverge <cduverge@student.42.fr>
2021-10-06 09:21:20 +0200

    parsing

--- parsing.py
+++ parsing.py
@@ -1,52 +1,86 @@
-#A LIRE
+import re
+
+#retourne le nom de la variable s'il est valide
+def extract_var(input):
+    rgx = "^[a-z]+"
+    match = re.search(rgx, input)
+    if match is not None:
+        return match.group(0)
+    else:
+        return None
+
+#verifie le nom de la fonction et le renvoie
+def extract_function(input):
+    rgx = "^[a-z]+\((.+)\)"
+    if not re.match(rgx, input):
+        return None
+    index = input.find('(')
+    bracket_count = 0
+    while index < len(input):
+        if input[index] == '(':
+            bracket_count += 1
+        elif input[index] == ')':
+            bracket_count -= 1
+        if bracket_count == 0:
+            break
+        index += 1
+    if bracket_count != 0:
+        print("\033[91mERREUR: Les parentheses sont mal agencees.\033[0m")
+        raise Exception
+    return input[:index + 1]
+
+#parse l'input et verifie s'il s'agit d'une assignation de fonction, de variable ou d'une equation a resoudre
 def parse(input):
-    function_regex = re.compile('^([a-z]+)\((.+)\)=')
-    parse_info = {'assign': False, 'assign_func': False, 'resolve_equat': False}
+    #regexp pour chercher des fonctions
+    fncRe = re.compile('^([a-z]+)\((.+)\)=')
+    tpeInfo = {'assign': False, 'assign_func': False, 'resolve_equat': False}
     if not input:
-        print('\033[91mError: Empty input\033[0m')
+        print('\033[91mERREUR: La ligne est vide.\033[0m')
         raise Exception
     if input.count("=") == 0:
-        print("\033[91mError: No equal\033[0m")
+        print("\033[91mERREUR: Il n'y a pas de '='.\033[0m")
         raise Exception
     if not input.endswith('?'):
-        parse_info['assign'] = True
-        match = re.match(function_regex, input)
+        tpeInfo['assign'] = True
+        match = re.match(fncRe, input)
+        #assigne une fonction
         if match is not None:
             tmp = extract_function(input)
-            parse_info['assign_func'] = match.group(1)
-            parse_info['var'] = match.group(2)
+            tpeInfo['assign_func'] = match.group(1)
+            tpeInfo['var'] = match.group(2)
             if input[len(tmp)] != "=":
-                print("\033[91mError: Something between variable name and =\033[0m")
+                print("\033[91mERREUR: Nom de la fonction non conforme.\033[0m")
                 raise Exception
+        #assigne une variable
         elif input[0].isalpha():
             tmp = extract_var(input)
             if tmp == 'i':
                 print("\033[91mError: Can't assign \"i\"\033[0m")
                 raise Exception
-            parse_info['assign'] = tmp
+            tpeInfo['assign'] = tmp
             if len(input) == len(tmp) or input[len(tmp)] != "=":
-                print("\033[91mError: Something between variable name and =\033[0m")
+                print("\033[91mERREUR: Nom de la variable non conforme.\033[0m")
                 raise Exception
         else:
-            print("\033[91mError: Can't find a variable to assign\033[0m")
+            print("\033[91mERREUR: Il n'y a pas de variable a assigner.\033[0m")
             raise Exception
     elif not input.endswith('=?'):
-        parse_info['resolve_equat'] = True
+        tpeInfo['resolve_equat'] = True
     if input.count("=") > 1:
-        print("\033[91mError: More than one equal\033[0m")
+        print("\033[91mERREUR: Il ne faut qu'un seul '=' dans l'entree.\033[0m")
         raise Exception
-    return parse_info
+    return tpeInfo
 
 #lance le parsing et recupere le type de recherche a faire
 def parsing(input):
     tpe = parse(input)
 
-    if tpe['assign_func']:
-        assign_func(input, tpe)
-    elif tpe['assign']:
-        assign_resolve(input, tpe)
-    elif tpe['resolve_equat']:
-        resolve_equat(input)
-    else:
-        res = resolve(input)
-        print res
+    # if tpe['assign_func']:
+    #     assign_func(input, tpe)
+    # elif tpe['assign']:
+    #     assign_resolve(input, tpe)
+    # elif tpe['resolve_equat']:
+    #     resolve_equat(input)
+    # else:
+    #     res = resolve(input)
+    #     print res
